1dfdb4b447ca74f60e4ae57fd77e4825df0fd7dc
Kshetra Mohan Prusty <kshetra03@users.noreply.github.com>
2018-05-11 23:23:18 +0530

    Update WebTable.java

--- WebTable.java
+++ WebTable.java
@@ -9,22 +9,28 @@ import java.util.List;
 import java.util.regex.Pattern;
 
 /**
-* Created by kprusty on 10/18/2017.
-*/
+ * Created by kprusty on 10/18/2017.
+ */
 public class WebTable_new extends Base{
 
-    By _table_locator ;
-    String _xpath_row_ng_repeat ;
+    static By _table_locator ;
+    static String _xpath_row_ng_repeat ;
 
     public WebTable_new(By _table_locator, String _xpath_row_ng_repeat){
         this._table_locator = _table_locator;
         this._xpath_row_ng_repeat = _xpath_row_ng_repeat;
     }
 
+    public static String get_cell_data(int row, int col){
+        int td ;
+        td = (row - 1)* get_table_column_count() + col ;
+        String _reqd_xapth = "("+ get_xpath_expression_from_By(_table_locator) + "//tr[@ng-repeat='"+_xpath_row_ng_repeat+"']/td)["+td+"]" ;
+        return driver.findElement(By.xpath(_reqd_xapth)).getText();
+    }
     /**
      * @return -- list of column headers of the table
      */
-    public List<String> get_all_table_column_headers(){
+    public static List<String> get_all_table_column_headers(){
         List<String> _col_headers = new ArrayList<>() ;
         for (WebElement we : driver.findElements(By.xpath(get_xpath_expression_from_By(_table_locator)+"//th"))){
             _col_headers.add(we.getText());
@@ -35,14 +41,14 @@ public class WebTable_new extends Base{
     /**
      * @return -- counts the number of columns in the table
      */
-    public int get_table_column_count(){
+    public static int get_table_column_count(){
         return get_all_table_column_headers().size();
     }
 
     /**
      * @return -- the number of rows present in the table
      */
-    public int get_table_row_count(){
+    public static int get_table_row_count(){
         int _curr_row_ctr ;
         _curr_row_ctr = driver.findElements(By.xpath(get_xpath_expression_from_By(_table_locator)+"//tr")).size();
         return _curr_row_ctr - 1 ; // minus 1 as there would be 1 row corresponding to table header
@@ -52,11 +58,11 @@ public class WebTable_new extends Base{
      * @param _col_name
      * @return -- position of column in the table
      */
-    public int get_table_column_position(String _col_name){
+    public static int get_table_column_position(String _col_name){
         return get_all_table_column_headers().indexOf(_col_name) + 1 ;// adding 1 as List is 0 based index
     }
 
-    public int get_table_row_num_based_on_search_keyword(String col_name, String _comp_name){
+    public static int get_table_row_num_based_on_search_keyword(String col_name, String _comp_name){
         return get_all_values_by_column_name(col_name).indexOf(_comp_name) + 2;    //adding 2 as List is 0 based and the 1st row is header
     }
 
@@ -64,7 +70,7 @@ public class WebTable_new extends Base{
      *  the tr changes in all table implementations when getting all the values from a column.
      *  This tr-repeat pattern can be used to make a generic API for this
      */
-    public List<String> get_all_values_by_column_name(String _col_name){
+    public static List<String> get_all_values_by_column_name(String _col_name){
         // extract position of the column i.e. ComponentName = 1
         List<String> _val_col_wise = new ArrayList<>();
         int _col_position = get_table_column_position(_col_name) ;
@@ -86,7 +92,7 @@ public class WebTable_new extends Base{
      * @param _cell_val
      * @return - xpath for the cell
      */
-    public String generate_xpath_expression_dynamically(String _col_name, String _cell_val){
+    public static String generate_xpath_expression_dynamically(String _col_name, String _cell_val){
         int _col_position = get_table_column_position(_col_name) ;
         int _num_of_cols = get_table_column_count();
         String _xpath_builder;
@@ -100,12 +106,12 @@ public class WebTable_new extends Base{
         return null;
     }
 
-    public String get_xpath_expression_from_By(By xpath){
+    public static String get_xpath_expression_from_By(By xpath){
         String[] _xpath_split = xpath.toString().split(":");
         return _xpath_split[1].trim();
     }
 
-    public void click_select_cell_in_table(String _col_name, String _cell_val) {
+    public static void click_select_cell_in_table(String _col_name, String _cell_val) {
         String _xpath = generate_xpath_expression_dynamically(_col_name, _cell_val);
         driver.findElement(By.xpath(_xpath)).click();
     }
@@ -119,7 +125,7 @@ public class WebTable_new extends Base{
      * @return - value from a column mapped to a different column's value
      * limitation - if _primary_col_header is ahead of the query col, this method wil not be applicable. As of now this situation has not come
      */
-    public String get_mapped_value(String _primary_col_header, String _primary_col_val, String _query_col){
+    public static String get_mapped_value(String _primary_col_header, String _primary_col_val, String _query_col){
         int _td_offset = Math.abs(get_table_column_position(_primary_col_header) - get_table_column_position(_query_col));
         String _xpath_of_primary_cell_val = generate_xpath_expression_dynamically(_primary_col_header, _primary_col_val);
         String[] a = _xpath_of_primary_cell_val.split(Pattern.quote("["));
